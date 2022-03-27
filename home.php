b19715ed4decf5e6803982d21ed3c581f63e38ea
Bruwaert Theo <bruwaert@myserver.org>
2022-03-27 22:33:59 +0200

    Changement de l'onglet Home à la connexion

--- home.php
+++ home.php
@@ -10,6 +10,7 @@
         href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
         rel="stylesheet"
     >
+    <link rel="stylesheet" href="assets/style.css">
 </head>
 <body class="d-flex flex-column min-vh-100">
     <div class="container">
@@ -19,23 +20,27 @@
 
     <!-- Formulaire de connexion -->
     <?php include_once('login.php'); ?>
-        <h1>Site de Recettes !</h1>
 
         <!-- Plus facile à lire -->
-        <?php foreach(get_recipes($recipes, $limit) as $recipe) : ?>
-            <article>
-                <h3><?php echo($recipe['title']); ?></h3>
-                <div><?php echo($recipe['recipe']); ?></div>
-                <i><?php echo(display_author($recipe['author'], $users)); ?></i>
-                <?php if(isset($loggedUser) && $recipe['author'] === $loggedUser['email']): ?>
-                    <ul class="list-group list-group-horizontal">
-                        <li class="list-group-item"><a class="link-warning" href="update.php?id=<?php echo($recipe['recipe_id']); ?>">Editer l'article</a></li>
-                        <li class="list-group-item"><a class="link-danger" href="delete.php?id=<?php echo($recipe['recipe_id']); ?>">Supprimer l'article</a></li>
-                        <li class="list-group-item"><a class="link-info" href="read.php?id=<?php echo($recipe['recipe_id']); ?>">Lire l'article</a></li>
-                    </ul>
-                <?php endif; ?>
-            </article>
-        <?php endforeach ?>
+        <?php if(isset($loggedUser)): ?>
+
+            <h1>Site de Recettes !</h1>
+
+            <?php foreach(get_recipes($recipes, $limit) as $recipe) : ?>
+                <article>
+                    <h3><?php echo($recipe['title']); ?></h3>
+                    <div><?php echo($recipe['recipe']); ?></div>
+                    <i><?php echo(display_author($recipe['author'], $users)); ?></i>
+                    <?php if(isset($loggedUser) && $recipe['author'] === $loggedUser['email']): ?>
+                        <ul class="list-group list-group-horizontal">
+                            <li class="list-group-item"><a class="link-warning" href="update.php?id=<?php echo($recipe['recipe_id']); ?>">Editer l'article</a></li>
+                            <li class="list-group-item"><a class="link-danger" href="delete.php?id=<?php echo($recipe['recipe_id']); ?>">Supprimer l'article</a></li>
+                            <li class="list-group-item"><a class="link-info" href="read.php?id=<?php echo($recipe['recipe_id']); ?>">Lire l'article</a></li>
+                        </ul>
+                    <?php endif; ?>
+                </article>
+            <?php endforeach ?>
+        <?php endif; ?>
     </div>
 
     <?php include_once('footer.php'); ?>
--- login.php
+++ login.php
@@ -50,23 +50,25 @@ if (isset($_SESSION['LOGGED_USER'])) {
 ?>
 
 <?php if(!isset($loggedUser)): ?>
-<form action="home.php" method="post">
-    <?php if(isset($errorMessage)) : ?>
-        <div class="alert alert-danger" role="alert">
-            <?php echo($errorMessage); ?>
-        </div>
-    <?php endif; ?>
-    <div class="mb-3">
-        <label for="email" class="form-label">Email</label>
-        <input type="email" class="form-control" id="email" name="email" aria-describedby="email-help" placeholder="you@exemple.com">
-        <div id="email-help" class="form-text">L'email utilisé lors de la création de compte.</div>
+    <div class="card-login">
+        <form action="home.php" method="post">
+            <?php if(isset($errorMessage)) : ?>
+                <div class="alert alert-danger" role="alert">
+                    <?php echo($errorMessage); ?>
+                </div>
+            <?php endif; ?>
+            <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input type="email" class="form-control" id="email" name="email" aria-describedby="email-help" placeholder="you@exemple.com">
+                <div id="email-help" class="form-text">L'email utilisé lors de la création de compte.</div>
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Mot de passe</label>
+                <input type="password" class="form-control" id="password" name="password">
+            </div>
+            <button type="submit" class="btn btn-primary">Envoyer</button>
+        </form>
     </div>
-    <div class="mb-3">
-        <label for="password" class="form-label">Mot de passe</label>
-        <input type="password" class="form-control" id="password" name="password">
-    </div>
-    <button type="submit" class="btn btn-primary">Envoyer</button>
-</form>
 <?php else: ?>
     <div class="alert alert-success" role="alert">
         Bonjour <?php echo($loggedUser['email']); ?> !
