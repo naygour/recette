120378a02564cea7bee595fe2cb760a04a13b3e8
Bruwaert Theo <bruwaert@myserver.org>
2022-03-28 22:44:01 +0200

    Modif login

--- logout.php
+++ logout.php
@@ -1,12 +1,12 @@
 <?php
 session_start();
 
-session_destroy();
-
 $file = 'compteur.txt';
 $monfichier = fopen($file, 'r+');
 ftruncate($monfichier,0);
 fclose($monfichier);
+
+session_destroy();
 
 HEADER('Location: home.php');
 
